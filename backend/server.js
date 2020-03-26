7d8786c5c49463fb741c74d459a0874b0e10498d
Wisdow <sacredsource71@gmail.com>
2020-03-26 15:58:46 +0000

    MERN upload

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.envtPORT || 5000;
+
+
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true ,useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open',() => {
+  console.log("MongoDB database connection open successfully");
+})
+
+app.use(cors());
+app.use(express.json());
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
